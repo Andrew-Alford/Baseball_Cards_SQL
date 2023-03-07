a22003f369daedf74b177bc92686827b539ad2cd
Ryan Summers <summers.ryanc@gmail.com>
2023-03-07 16:34:45 -0700

    First project update

---
+++ bball_card_db.sql
@@ -0,0 +1,108 @@
+
+/* use master
+go
+drop database baseball_cards_db
+go */
+
+/* create database baseball_cards_db
+GO  */
+
+
+use baseball_cards_db
+GO
+
+-- Down / Reset
+drop table if exists bills_cards
+drop table if exists fielders
+drop table if exists pitchers
+drop table if exists player_stats_by_year
+drop table if exists teams
+drop table if exists players_attributes
+
+
+-- create tables
+create table players_attributes (
+    player_id int identity primary key,
+    player_firstname varchar(50) not NULL,
+    player_lastname varchar(50) not null,
+    player_birthdate date null,
+    player_deathdate date null,
+    player_height int null,
+    player_weight int null,
+    player_bats int null,
+    player_throws varchar(5)
+)
+
+create table teams (
+    team_id int identity primary key,
+    team_name varchar(50) not null,
+    team_park varchar(50) not null,
+    team_year date not null
+        check (team_year >= 1970 and team_year <= 1989),
+    team_league varchar(50),
+    team_division_winner char(1) not NULL
+        check (team_division_winner in ('Y','N')),
+    team_wild_card_winner char(1) not NULL
+        check (team_wild_card_winner in ('Y','N')),
+    team_league_champion char(1) not NULL
+        check (team_league_champion in ('Y','N')),
+    team_world_series_chamption char(1) not NULL
+        check (team_league_champion in ('Y','N'))
+)
+
+create table players_stats_by_year (
+    stats_id int identity primary key,
+    player_id int not null foreign key references players_attributes (player_id),
+    team_id int not null foreign key references teams (team_id),
+    stats_year year not null,
+    stats_league varchar(50) not null,
+    stats_games int not null,
+    stats_position varchar(2) not null,
+    stats_at_bat int, 
+    stats_wins int,
+    stats_losses int,
+    stats_runs int,
+    stats_hits int,
+    stats_doubles int,
+    stats_triples int,
+    stats_home_runs int,
+    stats_rbi int,
+    stats_stolen_bases int,
+    stats_hit_by_pitch int,
+    stats_sac_hits int,
+    stats_sac_flies int,
+    stats_grd_dbl_play int,
+    stats_time_played int,
+    stats_putouts int,
+    stats_assists int,
+    stats_errors int,
+    stats_dbl_plays int,
+    stats_passed_balls_c int,
+    stats_wild_pitches_c int,
+    stats_opp_stolen_base_c int,
+    stats_strikeouts int, 
+    stats_opp_batting_avg float,
+    stats_games_started int,
+    stats_completed_games int,
+    stats_shutouts int,
+    stats_saves int,
+    stats_era float,
+    stats_intentional_walk int,
+    stats_wild_pitches int,
+    stats_batters_hit_by_pitch int,
+    stats_runs_allowed int
+)
+
+create table pitchers (
+    pitcher_id int identity primary key,
+    player_id int not null foreign key references players_attributes (player_id)
+)
+
+create table fielders (
+
+)
+
+create table bills_cards (
+
+)
+
